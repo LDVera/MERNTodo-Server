5696636501b8ffabb5fca85210e5452e2c986670
Diego <ldvera41@gmail.com>
2021-06-23 16:11:43 -0500

    fixing

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors())
 app.use(express.json({ extended: true }))
 
 // app port
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // import routes
 app.use('/api/users', require('./routes/users.js'))
@@ -32,6 +32,6 @@ app.use('/api/auth', require('./routes/auth.js'))
 app.use('/api/projects', require('./routes/projects.js'))
 app.use('/api/task', require('./routes/task.js'))
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`el servidor esta trabajando en el puerto ${port}`)
 })
